4bb0f8f0c6dac61539f8e688bb24e62d1e3e088d
Keyrune <38364152+KeyruneDeasy@users.noreply.github.com>
2026-03-07 09:31:15 +0200

    Crash fix in local_file_manager

--- Scripts/Editing/local_file_manager.gd
+++ Scripts/Editing/local_file_manager.gd
@@ -7,7 +7,7 @@ var files: Array[SessionResourceFile]
 
 func register_or_get_local_file(file_path: String) -> SessionResourceFile:
 	for file: SessionResourceFile in files:
-		if file.file_local_path == file_path:
+		if file._file_local_path == file_path:
 			return file
 	var new_file: SessionResourceFile = SessionResourceFile.new(file_path)
 	files.append(new_file)
